38635647969661c06d95a431d2eb913fef98eaf5
flabbet <flubbet@gmail.com>
2019-11-03 16:44:37 +0100

    Created topper. Container wip

---
+++ src/topNavController.js
@@ -0,0 +1,8 @@
+function toogleResponsive(){
+    var x = document.querySelector("#myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
